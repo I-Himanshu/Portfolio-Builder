1955f8349375c2c1382838e6a4133c2962da2725
i-himanshu <himanshu.kumar5403@gmail.com>
2024-05-07 15:55:31 +0530

    Updated /Home To /Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <Routes>
 
             <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
+              <Route index element={<Dashboard />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/users" element={<Users />} />
               <Route path="/create" element={
